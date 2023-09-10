df2c5148e98ed6254bc6e081bd4840140812557b
FW <35238548+AllSeasons1867@users.noreply.github.com>
2023-09-09 19:37:40 -0600

    Uploaded project from REPL.

---
+++ index.js
@@ -0,0 +1,41 @@
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection == computerSelection) {
+    console.log("Tie!");
+  } else if ((playerSelection == "rock" && computerSelection == "scissors") || (playerSelection == "paper" && computerSelection == "rock") || (playerSelection == "scissors" && computerSelection == "paper")) {
+    playerScore += 1;
+    console.log("Human player wins!");
+  } else {
+    computerScore += 1; 
+    console.log("Computer player wins!");
+  }
+};
+
+const getComputerChoice = () => {
+  const choices = ["rock", "paper", "scissors"];
+  return choices[Math.floor(Math.random() * 3)]
+};
+
+const getPlayerChoice = () => {
+  let playerChoice = '';
+  while (playerChoice != 'rock' && playerChoice != 'scissors' && playerChoice != 'paper') {
+      playerChoice = prompt("Choose 'rock', 'paper', or 'scissors'.").toLowerCase();
+  }
+  return playerChoice;
+}
+
+let playerScore = 0
+let computerScore = 0
+
+let playerSelection = getPlayerChoice();
+let computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
+playerSelection = getPlayerChoice();
+computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
+playerSelection = getPlayerChoice();
+computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
+console.log(`Player Score: ${playerScore} | Computer Score: ${computerScore}`)
