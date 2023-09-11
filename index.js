97cd0f8b9fbbaf893526f47234528276ac9808e3
FW <35238548+AllSeasons1867@users.noreply.github.com>
2023-09-10 20:50:58 -0600

    Updated game with GUI and 5 victory match limit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let playerScore = 0
 let computerScore = 0
+let round = 1
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection == computerSelection) {
@@ -26,4 +27,51 @@ const getPlayerChoice = () => {
   return playerChoice;
 }
 
-console.log(`Player Score: ${playerScore} | Computer Score: ${computerScore}`)
+// console.log(`Player Score: ${playerScore} | Computer Score: ${computerScore}`)
+
+let choices = document.querySelectorAll('.choices');
+
+const clearGame = () => {
+  let resultList = document.querySelector('.results')
+  resultList.innerHTML = ""
+  playerScore = 0
+  computerScore = 0
+  let score = document.querySelector('#score')
+  score.innerHTML = `User score: ${playerScore} | Computer score: ${computerScore}`
+}
+
+const updateScore = () => {
+  let score = document.querySelector('#score')
+
+  score.innerHTML = `User score: ${playerScore} | Computer score: ${computerScore}`
+
+  if (playerScore >= 5) {
+    alert("The player has won the match!")
+    clearGame()
+  } else if (computerScore >= 5) {
+    alert("The computer has a won the match!")
+    clearGame()
+  }
+}
+
+choices.forEach((choice) => {
+  choice.addEventListener('click', (event) => {
+    let playerSelection = event.target.getAttribute('value')
+    let computerSelection = getComputerChoice()
+    let resultList = document.querySelector('.results')
+
+    if (playerSelection == computerSelection) {
+      console.log("Tie!");
+      resultList.innerHTML += `<div>Tie!</div>`
+    } else if ((playerSelection == "rock" && computerSelection == "scissors") || (playerSelection == "paper" && computerSelection == "rock") || (playerSelection == "scissors" && computerSelection == "paper")) {
+      playerScore += 1;
+      resultList.innerHTML += `<div>User wins!</div>`
+    } else {
+      computerScore += 1; 
+      resultList.innerHTML += `<div>Computer wins!</div>`
+    }
+
+    updateScore();
+  })
+});
+
